6904a48e2cddd339a54ee52ea7411bf80cf96496
isole-01 <captain.atr007@gmail.com>
2022-12-07 15:49:54 +0330

    Initial commit

---
+++ workshop/src/main/java/com/java/workshop/model/list/MyIdList.java
@@ -0,0 +1,15 @@
+package com.java.workshop.model.list;
+
+import java.util.ArrayList;
+
+public class MyIdList {
+    private ArrayList<Long> ids;
+
+    public ArrayList<Long> getIds() {
+        return ids;
+    }
+
+    public void setIds(ArrayList ids) {
+        this.ids = ids;
+    }
+}
---
+++ workshop/src/main/java/com/java/workshop/model/workshop/Temp.java
@@ -0,0 +1,19 @@
+package com.java.workshop.model.workshop;
+
+import java.util.ArrayList;
+
+public class Temp {
+
+    ArrayList<Long> name = new ArrayList<>();
+
+    public Temp() {
+    }
+
+    public ArrayList<Long> getName() {
+        return name;
+    }
+
+    public void setName(ArrayList<Long> name) {
+        this.name = name;
+    }
+}
